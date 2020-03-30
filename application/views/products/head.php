d13fe7f440d186171c7e7f0f7ef69ea21f5afe4a
pujie <pw.prayitno@gmail.com>
2020-03-30 10:17:13 +0700

    update products head

--- application/views/products/head.php
+++ application/views/products/head.php
@@ -17,44 +17,44 @@
     
     <script type='text/javascript' src='/asset/aqua/js/jquery-1.7.min.js'></script>
     <script type='text/javascript' src='/asset/aqua/js/jquery-ui-1.8.min.js'></script>
-    <script type='text/javascript'src='/asset/aqua/js/plugins/jquery/jquery.mousewheel.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/jquery/jquery.mousewheel.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/cookie/jquery.cookies.2.2.0.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/cookie/jquery.cookies.2.2.0.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/bootstrap.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/bootstrap.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/charts/jquery.flot.js'></script>    
-    <script type='text/javascript'src='/asset/aqua/js/plugins/charts/jquery.flot.stack.js'></script>    
-    <script type='text/javascript'src='/asset/aqua/js/plugins/charts/jquery.flot.pie.js'></script>
-    <script type='text/javascript'src='/asset/aqua/js/plugins/charts/jquery.flot.resize.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/charts/jquery.flot.js'></script>    
+    <script type='text/javascript' src='/asset/aqua/js/plugins/charts/jquery.flot.stack.js'></script>    
+    <script type='text/javascript' src='/asset/aqua/js/plugins/charts/jquery.flot.pie.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/charts/jquery.flot.resize.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/sparklines/jquery.sparkline.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/sparklines/jquery.sparkline.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/fullcalendar/fullcalendar.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/fullcalendar/fullcalendar.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/select2/select2.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/select2/select2.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/uniform/uniform.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/uniform/uniform.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/maskedinput/jquery.maskedinput-1.3.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/maskedinput/jquery.maskedinput-1.3.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/validation/languages/jquery.validationEngine-en.js' charset='utf-8'></script>
-    <script type='text/javascript'src='/asset/aqua/js/plugins/validation/jquery.validationEngine.js' charset='utf-8'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/validation/languages/jquery.validationEngine-en.js' charset='utf-8'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/validation/jquery.validationEngine.js' charset='utf-8'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js'></script>
-    <script type='text/javascript'src='/asset/aqua/js/plugins/animatedprogressbar/animated_progressbar.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/animatedprogressbar/animated_progressbar.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/qtip/jquery.qtip-1.0.0-rc3.min.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/qtip/jquery.qtip-1.0.0-rc3.min.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/cleditor/jquery.cleditor.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/cleditor/jquery.cleditor.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/dataTables/jquery.dataTables.min.js'></script>    
+    <script type='text/javascript' src='/asset/aqua/js/plugins/dataTables/jquery.dataTables.min.js'></script>    
     
-    <script type='text/javascript'src='/asset/aqua/js/plugins/fancybox/jquery.fancybox.pack.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins/fancybox/jquery.fancybox.pack.js'></script>
     
-    <script type='text/javascript'src='/asset/aqua/js/cookies.js'></script>
-    <script type='text/javascript'src='/asset/aqua/js/actions.js'></script>
-    <script type='text/javascript'src='/asset/aqua/js/charts.js'></script>
-    <script type='text/javascript'src='/asset/aqua/js/plugins.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/cookies.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/actions.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/charts.js'></script>
+    <script type='text/javascript' src='/asset/aqua/js/plugins.js'></script>
     
 </head>
